2a53c613cbb36d7d71dcde2db9c74c0dad61b9a4
aktonkin <alice.k.tonkin@gmail.com>
2021-03-15 16:05:16 +1100

    Initial commit

---
+++ src/webparts/customSrsApplication/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICustomSrsApplicationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CustomSrsApplicationWebPartStrings' {
+  const strings: ICustomSrsApplicationWebPartStrings;
+  export = strings;
+}
