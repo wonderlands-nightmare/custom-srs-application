0d4b98c6c765334109d179ce98b6c0b4ca5efe14
aktonkin <alice.k.tonkin@gmail.com>
2021-03-16 15:24:59 +1100

    Reset initial commit as React rather than NoJS

---
+++ src/webparts/customSrsApplication/CustomSrsApplicationWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'CustomSrsApplicationWebPartStrings';
+import CustomSrsApplication from './components/CustomSrsApplication';
+import { ICustomSrsApplicationProps } from './components/ICustomSrsApplicationProps';
+
+export interface ICustomSrsApplicationWebPartProps {
+  description: string;
+}
+
+export default class CustomSrsApplicationWebPart extends BaseClientSideWebPart<ICustomSrsApplicationWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ICustomSrsApplicationProps> = React.createElement(
+      CustomSrsApplication,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
