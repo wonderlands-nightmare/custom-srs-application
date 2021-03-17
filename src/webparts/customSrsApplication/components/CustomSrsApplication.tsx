998e79dc216954a5ea1ebbd4e2c77538bc676757
aktonkin <alice.k.tonkin@gmail.com>
2021-03-17 13:37:08 +1100

    Modified config for scss and alias imports

--- src/webparts/customSrsApplication/components/CustomSrsApplication.tsx
+++ src/webparts/customSrsApplication/components/CustomSrsApplication.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import styles from './CustomSrsApplication.module.scss';
 import { ICustomSrsApplicationProps } from './ICustomSrsApplicationProps';
 import { escape } from '@microsoft/sp-lodash-subset';
